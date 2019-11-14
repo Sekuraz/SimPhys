0cfec0c9997001a33e827ef6c438be2f803f8b89
David Beyer <david.beyer@live.de>
2019-11-14 18:39:58 +0100

    Add solution for 2-3.2.

--- 2/solutions/ex_3_2.py
+++ 2/solutions/ex_3_2.py
@@ -4,15 +4,25 @@ import numpy as np
 import scipy.linalg
 
 
-def lj_potential(r_ij: np.ndarray) -> float:
+def lj_potential(r_ij):
+    return 4 * (1/np.linalg.norm(r_ij)**12 - 1/np.linalg.norm(r_ij)**6)
 
-    pass
-
-def lj_force(r_ij: np.ndarray) -> np.ndarray:
-
-    pass
+def lj_force(r_ij):
+    return 24.0 * (2.0/np.linalg.norm(r_ij)**14 - 1/np.linalg.norm(r_ij)**8) * r_ij
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    plt.rc('text', usetex=True)
 
-    pass
+    d = np.zeros((2, 1000))
+    d[0,:] = np.linspace(0.85, 2.5, 1000)
+
+    plt.plot(d[0,:], np.apply_along_axis(lj_potential, 0, d), 'b-')
+    plt.xlabel(r'$d$')
+    plt.ylabel(r'$V_\mathrm{LJ}(d)$')
+    plt.show()
+
+    plt.plot(d[0,:], np.apply_along_axis(lj_force, 0, d)[0], 'r-')
+    plt.xlabel(r'$d$')
+    plt.ylabel(r'$F_{\mathrm{LJ},x}(d)$')
+    plt.show()
