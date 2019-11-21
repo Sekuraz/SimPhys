cd8527cba4bfcc7bccd48e6fb3bfae9ca8b8adbf
Markus Baur <markusb@selfnet.de>
2019-11-21 17:30:13 +0100

    Add 3.5

--- 2/solutions/ex_3_5.py
+++ 2/solutions/ex_3_5.py
@@ -33,7 +33,13 @@ if __name__ == "__main__":
     # particle positions
 
     # SET UP THE PARTICLE POSITIONS ON A LATTICE HERE
-    # x = ...
+    x = np.full((2, N_PART), np.nan)
+    _box_start = BOX / (2 * N_PER_SIDE)
+    _box_x = _box_start * 2 * np.array((1, 0))
+    _box_y = _box_start * 2 * np.array((0, 1))
+    for i in range(N_PER_SIDE):
+        for j in range(N_PER_SIDE):
+            x[:, i * N_PER_SIDE + j] = _box_start + i * _box_x + j * _box_y
 
     # random particle velocities
     v = 2.0 * np.random.random((2, N_PART)) - 1.0
@@ -53,3 +59,21 @@ if __name__ == "__main__":
 
     end_time = time.time()
     print(f"{N_PART}\t{end_time - start_time}")
+
+    # our output
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    for i in range(N_PART):
+        ax1.plot(positions[:, 0, i], positions[:, 1, i], label='{}'.format(i))
+    ax1.set_title('Trajectory')
+    ax1.set_aspect('equal')
+    ax1.set_xlabel('x position')
+    ax1.set_ylabel('y position')
+    ax1.legend()
+
+    ax2.set_xlabel("Time step")
+    ax2.set_ylabel("Total energy")
+    ax2.plot(energies)
+    ax2.set_title('Total energy')
+    plt.show()
+
