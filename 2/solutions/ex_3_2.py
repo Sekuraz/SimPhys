f3e8b95552db766df571d5f363d3421423da71fc
David Beyer <david.beyer@live.de>
2019-11-21 17:25:37 +0100

    Add solutions for thermodynamic exercises. Modify Lennard-Jones functions.

--- 2/solutions/ex_3_2.py
+++ 2/solutions/ex_3_2.py
@@ -5,10 +5,14 @@ import scipy.linalg
 
 
 def lj_potential(r_ij):
-    return 4 * (1/np.linalg.norm(r_ij)**12 - 1/np.linalg.norm(r_ij)**6)
+    ret = 4 * (1/np.linalg.norm(r_ij)**12 \
+        - 1/np.linalg.norm(r_ij)**6)
+    return ret
 
 def lj_force(r_ij):
-    return 24.0 * (2.0/np.linalg.norm(r_ij)**14 - 1/np.linalg.norm(r_ij)**8) * r_ij
+    ret = 24.0 * (2.0/np.linalg.norm(r_ij)**14 \
+        - 1/np.linalg.norm(r_ij)**8) * r_ij
+    return ret
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -18,11 +22,11 @@ if __name__ == "__main__":
     d[0,:] = np.linspace(0.85, 2.5, 1000)
 
     plt.plot(d[0,:], np.apply_along_axis(lj_potential, 0, d), 'b-')
-    plt.xlabel(r'$d$')
-    plt.ylabel(r'$V_\mathrm{LJ}(d)$')
+    plt.xlabel(r'$d/\sigma$')
+    plt.ylabel(r'$V_\mathrm{LJ}(d)/\epsilon$')
     plt.show()
 
     plt.plot(d[0,:], np.apply_along_axis(lj_force, 0, d)[0], 'r-')
-    plt.xlabel(r'$d$')
-    plt.ylabel(r'$F_{\mathrm{LJ},x}(d)$')
+    plt.xlabel(r'$d/\sigma$')
+    plt.ylabel(r'$F_{\mathrm{LJ},x}(d)\cdot\frac{\sigma}{\epsilon}$')
     plt.show()
