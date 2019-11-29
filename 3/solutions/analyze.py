7d950277b913e3c4b7c16da1f841fb9313a39762
Markus Baur <markusb@selfnet.de>
2019-11-29 14:49:10 +0100

    Add average after t_eq.

--- 3/solutions/analyze.py
+++ 3/solutions/analyze.py
@@ -8,12 +8,30 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help="Path to pickle file.")
+parser.add_argument('-t', '--t-eq', type=int, help="Path to pickle file.")
+
 args = parser.parse_args()
 
 with open(args.file, 'rb') as fp:
     data = pickle.load(fp)
 
-print(len(data['energies']))
 
 def running_average(O, M):
     pass
+
+
+observables = ['energies', 'pressures', 'temperatures']
+
+
+def print_teq():
+    if not hasattr(args, "t_eq"):
+        return
+    for o in observables:
+        d = data[o]
+        if len(d) < args.t_eq:
+            return
+        d = d[args.t_eq:]
+        print("Average equilibrium %s: %s" % (o, sum(d) / len(d)))
+
+
+print_teq()
