25a522331cd5705164ed1a889be64c6caeaf1359
Markus Baur <markusb@selfnet.de>
2019-12-05 17:22:15 +0100

    Add random positions, force limiting and temperature rescaling.

--- 3/solutions/lj.py
+++ 3/solutions/lj.py
@@ -2,13 +2,14 @@
 
 
 class Simulation:
-    def __init__(self, dt, x, v, box, r_cut, shift):
+    def __init__(self, dt, x, v, box, r_cut, shift, f_max):
         self.dt = dt
         self.x = x.copy()
         self.v = v.copy()
         self.box = box.copy()
         self.r_cut = r_cut
         self.shift = shift
+        self.f_max = f_max
 
         self.n_dims = self.x.shape[0]
         self.n = self.x.shape[1]
@@ -47,10 +48,11 @@ class Simulation:
         for dim in range(self.n_dims):
             with np.errstate(invalid='ignore'):
                 self.f_ij_matrix[:, :, dim] *= np.where(r != 0.0, fac / r, 0.0)
-        self.f = np.sum(self.f_ij_matrix, axis=0).transpose()
+        f = np.sum(self.f_ij_matrix, axis=0).transpose()
+        self.f = np.clip(f, - self.f_max, self.f_max)
 
     def e_pot(self):
-        return np.sum(self.e_pot_ij_matrix)
+        return np.sum(self.e_pot_ij_matrix) / 2
 
     def e_kin(self):
         return np.sum(0.5 * np.power(self.v, 2))  # mass = 1
@@ -71,23 +73,29 @@ class Simulation:
     def pressure(self):
         def ideal():
             return self.e_kin() * 2 / (self.n_dims * np.product(self.box))
-        s = 0
-        for i in range(1, self.n):
-            for j in range(i):
-                s += np.dot(self.f_ij_matrix[i][j], self.r_ij_matrix[i][j])
-        # f = np.multiply(self.f_ij_matrix, self.r_ij_matrix)
-        # f = np.abs(f)  # all the values should be positive
-        # f = np.sum(f)
-        f = s * 2
+        # s = 0
+        # for i in range(1, self.n):
+        #     for j in range(i):
+        #         s += np.dot(self.f_ij_matrix[i][j], self.r_ij_matrix[i][j])
+        # f = s * 2
+
+        f = np.multiply(self.f_ij_matrix, self.r_ij_matrix)
+        f = np.sum(f)
+
         area = np.product(self.box)
-        ret = 1 / area * self.e_kin() + f * 0.25
-        print("Pressure: calculated=%4.6f, ideal=%4.6f" % (ret, ideal()))
+        ret = 1 / area * (self.e_kin() + f * 0.25)
+        # print("Pressure: calculated=%4.6f, ideal=%4.6f" % (ret, ideal()))
         return ret
 
     def rdf(self):
         r = np.linalg.norm(self.r_ij_matrix, axis=2)
         hist, bins = np.histogram(r, bins=100, range=(0.8, 5))
         return hist, bins
+
+    def rescale(self, T0):
+        temp = self.temperature()
+        factor = np.sqrt(T0 / temp)
+        self.v *= factor
 
     def propagate(self):
         # update positions
@@ -134,12 +142,18 @@ if __name__ == "__main__":
         '--cpt',
         type=str,
         help='Path to checkpoint.')
+    parser.add_argument(
+        '-t',
+        '--temperature',
+        type=float,
+        default=1.0,
+        help='Target temperature.')
     args = parser.parse_args()
 
     np.random.seed(2)
 
     DT = 0.01
-    T_MAX = 100.0
+    T_MAX = 10.0
     N_TIME_STEPS = int(T_MAX / DT)
 
     R_CUT = 2.5
@@ -157,8 +171,12 @@ if __name__ == "__main__":
     if not args.cpt or not os.path.exists(args.cpt):
         logging.info("Starting from scratch.")
         # particle positions
-        x = np.array(list(itertools.product(np.linspace(0, BOX[0], N_PER_SIDE, endpoint=False),
-                                            np.linspace(0, BOX[1], N_PER_SIDE, endpoint=False)))).T
+        # x = np.array(list(itertools.product(np.linspace(0, BOX[0], N_PER_SIDE, endpoint=False),
+        #                                     np.linspace(0, BOX[1], N_PER_SIDE, endpoint=False)))).T
+
+        x = np.random.random((DIM, N_PART))
+        for i in range(DIM):
+            x[i] *= BOX[i]
 
         # random particle velocities
         v = 0.5*(2.0 * np.random.random((DIM, N_PART)) - 1.0)
@@ -168,6 +186,7 @@ if __name__ == "__main__":
         pressures = []
         temperatures = []
         rdfs = []
+        f_max = 20
     #elif args.cpt and os.path.exists(args.cpt):
     else:
         logging.info("Reading state from checkpoint.")
@@ -182,9 +201,9 @@ if __name__ == "__main__":
         x = state['x']
         v = state['v']
         f = state['f']
+        f_max = state['f_max']
 
-
-    sim = Simulation(DT, x, v, BOX, R_CUT, SHIFT)
+    sim = Simulation(DT, x, v, BOX, R_CUT, SHIFT, f_max)
 
     # If checkpoint is used, also the forces have to be reloaded!
     if args.cpt and os.path.exists(args.cpt):
@@ -200,6 +219,11 @@ if __name__ == "__main__":
             temperatures.append(sim.temperature())
             rdfs.append(sim.rdf())
 
+            sim.rescale(args.temperature)
+            sim.f_max *= 1.1
+            if sim.f_max >= 1e9:
+                sim.f_max = np.inf
+
     if args.cpt:
         state = {'positions': positions,
                  'energies': energies,
@@ -208,5 +232,6 @@ if __name__ == "__main__":
                  'rdfs': rdfs,
                  'x':sim.x,
                  'v': sim.v,
-                 'f': sim.f}
+                 'f': sim.f,
+                 'f_max': sim.f_max}
         write_checkpoint(state, args.cpt, overwrite=True)
